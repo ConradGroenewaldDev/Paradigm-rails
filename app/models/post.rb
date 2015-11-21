c8e75f3b7bda61e60fe7ec0273dec72d4ffefed6
Conrad Groenewald <conrad@Conrads-MacBook-Air.local>
2015-11-21 23:19:08 +0000

    added some styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments
-  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" },
+  :default_url => 'Images-icon.png'
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
